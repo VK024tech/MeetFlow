003e84b83875649d3a55c69b8f36daeff983beff
vivek <kandarivivek260@gmail.com>
2025-05-07 14:36:41 +0530

    input comp

--- Frontend/src/Components/ChatBox.tsx
+++ Frontend/src/Components/ChatBox.tsx
@@ -2,21 +2,29 @@ import type { JSX } from "react";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 
+
 function ChatBox() {
+
+    
+
+
+
+
+
   function chatFooter(): JSX.Element {
     return (
-      <div className="bg-gray-50 min-h-12 flex  ">
-        <div className="bg-red-300 p-1 inline-block self-center rounded-full">
-          <PlusIcon className="size-6 text-blue-500" />
+      <div className="bg-white outline-1 outline-gray-100 px-2 min-h-12 flex py-3  shadow-md shadow-gray-50">
+        <div className="bg-red-200 p-1 inline-block self-center rounded-full">
+          <PlusIcon className="size-6 text-white" />
         </div>
 
         <input
           type="text"
           placeholder="Start Typing..."
-          className=" w-full px-2 "
+          className=" w-full px-2 outline-none text-gray-800"
         />
-        <div className="self-center bg-red-300 flex flex-row items-center px-2 py-1.5 rounded-md gap-1">
-            <PaperAirplaneIcon className="inline-block size-6 "/>
+        <div className="self-center text-white bg-red-200 flex flex-row items-center px-2 py-1.5 rounded-md gap-1">
+          <PaperAirplaneIcon className="inline-block size-6 " />
           <span>Send</span>
         </div>
       </div>
@@ -25,7 +33,9 @@ function ChatBox() {
 
   return (
     <div className="px-2">
-      <div>{chatFooter()}</div>
+      <div>
+        {chatFooter()}
+        </div>
     </div>
   );
 }
