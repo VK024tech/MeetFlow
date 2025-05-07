f2dd27e6445b5ea59207e5e77c25b73b8690afd3
vivek <kandarivivek260@gmail.com>
2025-05-07 18:43:35 +0530

    add option animation

--- Frontend/src/Components/ChatBox.tsx
+++ Frontend/src/Components/ChatBox.tsx
@@ -1,4 +1,5 @@
 import { useState, type JSX } from "react";
+import { motion, AnimatePresence } from "motion/react";
 import {
   PlusIcon,
   SpeakerWaveIcon,
@@ -10,34 +11,38 @@ import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 function ChatBox() {
   const [share, setShare] = useState<boolean>(false);
 
-
   ///messages on screen
-  function chatScreen():JSX.Element{
-    return(
-        <div className="">
-
-        </div>
-    )
+  function chatScreen(): JSX.Element {
+    return <div className=""></div>;
   }
-
 
   //share option
   function shareOptions(): JSX.Element {
     return (
-      <ul className="w-fit gap-3 flex flex-wrap rounded-md text-gray-500 border-1 bg-white border-gray-100 absolute bottom-1 px-2 py-1 ">
-        <div className="flex gap-2 p-2 hover:text-red-200 hover:bg-red-50 rounded-md cursor-pointer transition-colors ">
-          <SpeakerWaveIcon className="size-6" />
-          <span>Audio</span>
-        </div>
-        <div className="flex gap-2 p-2 hover:text-red-200 hover:bg-red-50 rounded-md cursor-pointer transition-colors ">
-          <VideoCameraIcon className="size-6" />
-          <span>Video</span>
-        </div>
-        <div className="flex gap-2 p-2 hover:text-red-200 hover:bg-red-50 rounded-md cursor-pointer transition-colors">
-          <PhotoIcon className="size-6" />
-          <span>Image</span>
-        </div>
-      </ul>
+      <AnimatePresence>
+        {share && (
+          <motion.div
+            initial={{ opacity: 0, scaleY: 0 }}
+            animate={{ opacity: 1, scaleY: 1 }}
+            exit={{ opacity: 0, scaleY: 0 , transition:{duration: 0.1}  }}
+          >
+            <ul className="w-fit gap-3 flex flex-wrap rounded-md text-gray-500 border-1 bg-white border-gray-100 absolute bottom-1 px-2 py-1 ">
+              <div className="flex gap-2 p-2 py-1 hover:text-red-200 hover:bg-red-50 rounded-md cursor-pointer transition-colors ">
+                <SpeakerWaveIcon className="size-6" />
+                <span>Audio</span>
+              </div>
+              <div className="flex gap-2 p-2 py-1 hover:text-red-200 hover:bg-red-50 rounded-md cursor-pointer transition-colors ">
+                <VideoCameraIcon className="size-6" />
+                <span>Video</span>
+              </div>
+              <div className="flex gap-2 p-2 py-1 hover:text-red-200 hover:bg-red-50 rounded-md cursor-pointer transition-colors">
+                <PhotoIcon className="size-6" />
+                <span>Image</span>
+              </div>
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     );
   }
 
@@ -45,11 +50,14 @@ function ChatBox() {
   function chatFooter(): JSX.Element {
     return (
       <>
-        <div className="relative">{share && shareOptions()}</div>
+        <div className="relative">{shareOptions()}</div>
         <div className="bg-white outline-1 outline-gray-100 px-2 min-h-12 flex py-3  shadow-md shadow-gray-50">
-          <div onClick={()=>{
-            setShare(!share)
-          }} className="bg-red-200 hover:bg-red-300  transition-all cursor-pointer p-1 inline-block self-center rounded-full hover:outline outline-red-300">
+          <div
+            onClick={() => {
+              setShare(!share);
+            }}
+            className="bg-red-200 hover:bg-red-300  transition-all cursor-pointer p-1 inline-block self-center rounded-full hover:outline outline-red-300"
+          >
             <PlusIcon className="size-6 text-white" />
           </div>
 
@@ -69,7 +77,6 @@ function ChatBox() {
 
   return (
     <div className="px-2 h-dvh w-full">
-      
       <div className="h-[90%]">{chatScreen()}</div>
       <div>{chatFooter()}</div>
     </div>
