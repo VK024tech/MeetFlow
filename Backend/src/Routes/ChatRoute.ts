9f49826c8a3cd433ba5a1572de6bebdb01d3693c
Vivek Kandari <Kandarivivek260@gmail.com>
2025-05-13 18:33:15 +0530

    added websocket logic

--- Backend/src/Routes/ChatRoute.ts
+++ Backend/src/Routes/ChatRoute.ts
@@ -3,21 +3,23 @@ import { PrismaClient } from "../generated/prisma";
 
 import { env } from "../config/config";
 import * as jwt from "jsonwebtoken";
+import { boolean } from "zod";
+import { json } from "express";
 
 const prisma = new PrismaClient();
 ///websocket broadcast message
 
 interface data {
   username: string;
-  id: number;
+  userid: number;
   iat?: number;
 }
 
 function chatSocket(server: any) {
   const wss = new WebSocketServer({ server });
   let userCount = 0;
-  let allSockets: WebSocket[] = [];
-  wss.on("connection", (socket, req) => {
+  let allSockets: { [key: string]: WebSocket } = {};
+  wss.on("connection", async (socket, req) => {
     const token = req.headers.token as string;
 
     if (!token) {
@@ -29,18 +31,56 @@ function chatSocket(server: any) {
       const data = jwt.verify(token, env.SECRET) as data;
       try {
         console.log(data);
-        allSockets.push(socket);
+        allSockets[data.userid] = socket;
         userCount = userCount + 1;
         console.log("websocket connected");
         console.log(userCount);
-        socket.on("message", function message(message) {
-          allSockets.forEach((curr) => {
-            if (curr !== socket) {
-              curr.send(` ${message.toString()}`);
-            }
-          });
+
+        const currUser = Boolean(
+          await prisma.user.findUnique({
+            where: {
+              id: data.userid,
+            },
+          })
+        );
+
+        if (!currUser) {
+          socket.send(
+            JSON.stringify({ error: "No user(sender) with this id" })
+          );
+          socket.close();
+          return;
+        }
+
+        socket.on("message", async function message(message) {
+          const parsedMessage = JSON.parse(message.toString());
+
+          const receiver = Boolean(
+            await prisma.user.findUnique({
+              where: {
+                id: parsedMessage.sendTo,
+              },
+            })
+          );
+
+          if (!receiver) {
+            socket.send(
+              JSON.stringify({ error: "No user(reciever) with this id" })
+            );
+            socket.close();
+            return;
+          }
+
+          allSockets[parsedMessage.sendTo].send(
+            JSON.stringify(parsedMessage.message)
+          );
         });
-      } catch (error) {}
+      } catch (error) {
+        console.log("internal error");
+        socket.send(JSON.stringify({ error: "internal server error" }));
+        socket.close();
+        return;
+      }
     } catch (error) {
       console.log("cathc the error", error);
       socket.send(JSON.stringify({ error: "Invalid token" }));
